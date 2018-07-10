74ab95004808db2a0ca3b5b2bcc35432ef4cdd43
youngwoo.jin <tomcat1983@gmail.com>
2018-07-10 10:59:31 +0900

    Add travis ci configuration

---
+++ test/edu/cmu/tartan/TestMain.java
@@ -0,0 +1,16 @@
+package edu.cmu.tartan;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMain {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		System.out.println("TestMain!!");
+		assert(true);
+	}
+
+}
