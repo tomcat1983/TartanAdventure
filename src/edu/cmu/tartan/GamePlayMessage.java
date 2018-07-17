fbd77a1acf066874f0e5f2b49c32f2e27f4fa0cd
sibong.roh@gmail.com <sibong.roh@lge.com>
2018-07-17 13:06:47 +0900

    Code refactoring 1. GamePlayMessage.java change public interface to public final class.

--- src/edu/cmu/tartan/GamePlayMessage.java
+++ src/edu/cmu/tartan/GamePlayMessage.java
@@ -1,6 +1,6 @@
 package edu.cmu.tartan;
 
-public interface GamePlayMessage {
-	String I_DO_NOT_SEE_THAT_HERE = "I don't see that here.";
-	String TAKEN = "Taken";
+public final class GamePlayMessage {
+	public static final String I_DO_NOT_SEE_THAT_HERE = "I don't see that here.";
+	public static final String TAKEN = "Taken";
 }
