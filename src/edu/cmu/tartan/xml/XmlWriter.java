34b66367e62ccb35b248f8cd0a3fae06f6bdbeef
htak1 <40187539+htak1@users.noreply.github.com>
2018-07-23 03:12:06 +0900

    bug

--- src/edu/cmu/tartan/xml/XmlWriter.java
+++ src/edu/cmu/tartan/xml/XmlWriter.java
@@ -131,19 +131,19 @@ public class XmlWriter {
         //copy file conventional way using Stream
         InputStream is = null;
         OutputStream os = null;
-        try {
-			is = new FileInputStream(source);
-            os = new FileOutputStream(dest);
-            byte[] buffer = new byte[1024];
-            int length;
-            
-            while ((length = is.read(buffer)) > 0) {
-                os.write(buffer, 0, length);
-            }
-        } finally {
-        	is.close();
-            os.close();
+        
+        is = new FileInputStream(source);
+        os = new FileOutputStream(dest);
+        byte[] buffer = new byte[1024];
+        int length;
+
+        while ((length = is.read(buffer)) > 0) {
+        	os.write(buffer, 0, length);
         }
+
+        is.close();
+        os.close();
+    
 	}
 	
 	
