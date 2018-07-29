bbd4d674385a722d4b9bceb2cbc7fdc4d63e0c87
htak1 <40187539+htak1@users.noreply.github.com>
2018-07-30 04:47:43 +0900

    food should be installable

--- src/edu/cmu/tartan/item/ItemFood.java
+++ src/edu/cmu/tartan/item/ItemFood.java
@@ -2,6 +2,8 @@ package edu.cmu.tartan.item;
 
 import edu.cmu.tartan.properties.Edible;
 import edu.cmu.tartan.properties.Holdable;
+import edu.cmu.tartan.properties.Hostable;
+import edu.cmu.tartan.properties.Installable;
 import edu.cmu.tartan.properties.Meltable;
 
 /**
@@ -12,7 +14,7 @@ import edu.cmu.tartan.properties.Meltable;
  * Versions:
  * 1.0 March 2018 - initial version
  */
-public class ItemFood extends Item implements Edible, Holdable, Meltable {
+public class ItemFood extends Item implements Edible, Holdable, Meltable, Installable {
 	private Item hiddenItem = null;
 
     /**
