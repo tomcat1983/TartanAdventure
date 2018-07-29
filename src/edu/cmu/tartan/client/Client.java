1d8333853f5c6795d60d9b43e3c70f9d66c8f9be
youngwoo.jin <tomcat1983@gmail.com>
2018-07-29 00:35:19 -0400

    Fix sonar issue

--- src/edu/cmu/tartan/client/Client.java
+++ src/edu/cmu/tartan/client/Client.java
@@ -5,12 +5,11 @@ import java.util.logging.Logger;
 import edu.cmu.tartan.GameInterface;
 import edu.cmu.tartan.LocalGame;
 import edu.cmu.tartan.Player;
-import edu.cmu.tartan.config.Config;
 import edu.cmu.tartan.socket.SocketClient;
 import network.NetworkInterface;
 
 public class Client {
-	
+
 	/**
 	 * Game logger for game log
 	 */
@@ -20,37 +19,37 @@ public class Client {
 	 * Game interface for game log
 	 */
 	private GameInterface gameInterface = GameInterface.getInterface();
-	
+
 	/**
 	 * Client interface instance
 	 */
 	private ClientInterface clientInterface;
-	
+
 	/**
 	 * Socket to server
 	 */
 	SocketClient socket;
-	
+
 	/**
 	 * Socket thread
 	 */
 	Thread socketClientThread;
-	
+
 	/**
 	 * User ID
 	 */
 	String userId;
-	
+
 	public Client() {
 		clientInterface = new ClientInterface();
 	}
-	
+
 	public boolean start() {
 		boolean running = true;
 
 		do {
 			clientInterface.printGameModeMessage();
-			
+
 			switch (clientInterface.getRunningMode()) {
 			case LOCAL:
 				if (!runLocalMode())
@@ -75,7 +74,7 @@ public class Client {
 
 		return true;
 	}
-	
+
 	private boolean runLocalMode() {
 		clientInterface.printLocalModeMessage();
 		ClientInterface.LocalModeCommand command = clientInterface.getLocalModeCommand();
@@ -94,158 +93,173 @@ public class Client {
 
 		return false;
 	}
-	
+
 	private boolean continueGame() {
 		gameInterface.println("TBD");
 		LocalGame localGame = new LocalGame(Player.DEFAULT_USER_NAME);
 		if(localGame.loadAndStart(Player.DEFAULT_USER_NAME)) {
-			
+
 		}
 		return true;
 	}
-	
+
 	private boolean newGame() {
 		LocalGame localGame = new LocalGame(Player.DEFAULT_USER_NAME);
 		if(localGame.configureGame()) {
 			localGame.start();
 		}
-		
+
 		return true;
 	}
-	
+
 	private boolean connectServer(int timeout) {
-		String serverIp = Config.getServerIp();
-		int serverPort = Config.getServerPort();
-		
-		socket = new SocketClient(serverIp, serverPort);
-		socketClientThread = new Thread((Runnable)socket);
+		socket = new SocketClient();
+		socketClientThread = new Thread(socket);
 		socketClientThread.start();
-		
+
 		return socket.waitToConnection(timeout);
 	}
-	
+
 	private boolean disconnectServer() {
 		return socket.stopSocket();
 	}
-	
+
 	private boolean sendMessage(String message) {
 		return socket.sendMessage(message);
 	}
-	
+
 	private boolean login(boolean isDesigner) {
 		String id;
-		
+
 		do {
 			String[] loginInfo = clientInterface.getLoginInfo();
 			NetworkInterface.PacketType packetType = NetworkInterface.PacketType.LOGIN;
 			if (isDesigner)
 				packetType = NetworkInterface.PacketType.DESIGNER;
-			
+
 			id = loginInfo[0];
 			String[] data = { loginInfo[1] };
 			String loginPacket = NetworkInterface.makePacket(packetType, id, data);
-			
+
 			sendMessage(loginPacket);
 		} while (false); //TODO: need listener;
-		
+
 		userId = id;
 
 		return true;
 	}
-	
-	private boolean register() {
+
+	private String getUserIdForRegister() {
 		String id = "";
+
+		do {
+			id = clientInterface.getUserIdForRegister();
+			if (id.equals("")) //TODO: validate id
+				clientInterface.printInvalidIdMessageForRegister();
+			else
+				break;
+		} while(true);
+
+		return id;
+	}
+
+	private String getUserPwForRegister() {
 		String pw = "";
 		String confirmPw = "";
 
 		do {
 			do {
-				id = clientInterface.getUserIdForRegister();
-				if (id.equals("")) //TODO: validate id
-					clientInterface.printInvalidIdMessageForRegister();
+				pw = clientInterface.getUserPwForRegister();
+				if (pw.equals("")) //TODO: validate pw
+					clientInterface.printInvalidPwMessageForRegister();
 				else
 					break;
-			} while(true); 
-			
-			do {
-				do {
-					pw = clientInterface.getUserPwForRegister();
-					if (pw.equals("")) //TODO: validate pw
-						clientInterface.printInvalidPwMessageForRegister();
-					else
-						break;
-				} while(true);
-				
-				confirmPw = clientInterface.getUserMatchPwForRegister();
-				if (confirmPw.equals(pw))
-					break;
-				else
-					clientInterface.printUnmatchPwMessageForRegister();
 			} while(true);
-			
+
+			confirmPw = clientInterface.getUserMatchPwForRegister();
+			if (confirmPw.equals(pw))
+				break;
+			else
+				clientInterface.printUnmatchPwMessageForRegister();
+		} while(true);
+
+		return pw;
+	}
+
+	private boolean register() {
+		String id = "";
+		String pw = "";
+
+		do {
+			id = getUserIdForRegister();
+			pw = getUserPwForRegister();
+
 			NetworkInterface.PacketType packetType = NetworkInterface.PacketType.REGISTER;
 			String[] data = { pw };
-			
+
 			String registerPacket = NetworkInterface.makePacket(packetType, id, data);
-			
+
 			if (sendMessage(registerPacket)) //TODO: check register result
 				break;
 			else
-				clientInterface.printFailMessageForRegister();					
+				clientInterface.printFailMessageForRegister();
 		} while (true);
-		
+
 		clientInterface.printSuccessMessageForRegister();
-		
+
 		userId = id;
-		
+
 		return true;
 	}
-	
+
+	private boolean runNetworkCommander(boolean isDesigner) {
+		boolean running = true;
+
+		do {
+			String command = clientInterface.getNetworkCommand();
+
+			if (command.equals("quit")) {
+				running = false;
+			}
+			else if (isDesigner) {
+				if (command.equals("")) { //TODO: map check
+					clientInterface.printInvalidMap();
+				} else {
+					NetworkInterface.PacketType packetType = NetworkInterface.PacketType.MAP;
+					String[] data = { command };
+					String loginPacket = NetworkInterface.makePacket(packetType, userId, data);
+
+					sendMessage(loginPacket);
+				}
+			}
+			else {
+				NetworkInterface.PacketType packetType = NetworkInterface.PacketType.COMMAND;
+				String[] data = { command };
+				String loginPacket = NetworkInterface.makePacket(packetType, userId, data);
+
+				sendMessage(loginPacket);
+			}
+		} while (running);
+
+		return true;
+	}
+
 	private boolean runNetworkMode(boolean isDesigner) {
 		if (connectServer(1000)) {
-			ClientInterface.NetworkModeCommand networkCommand = ClientInterface.NetworkModeCommand.LOGIN; 
-					
+			ClientInterface.NetworkModeCommand networkCommand = ClientInterface.NetworkModeCommand.LOGIN;
+
 			if (!isDesigner)
-				networkCommand = clientInterface.getNetworkModeCommand();			
-			
+				networkCommand = clientInterface.getNetworkModeCommand();
+
 			switch (networkCommand) {
 			case LOGIN:
 				login(isDesigner);
 				clientInterface.printWelcomMessage(isDesigner);
-
-				boolean running = true;
-				
-				do {
-					String command = clientInterface.getNetworkCommand();
-					
-					if (command.equals("quit")) {
-						running = false;
-					}
-					else if (isDesigner) {
-						if (command.equals("")) { //TODO: map check
-							clientInterface.printInvalidMap();
-						} else {
-							NetworkInterface.PacketType packetType = NetworkInterface.PacketType.MAP;
-							String[] data = { command };
-							String loginPacket = NetworkInterface.makePacket(packetType, userId, data);
-							
-							sendMessage(loginPacket);
-						}
-					}
-					else {
-						NetworkInterface.PacketType packetType = NetworkInterface.PacketType.COMMAND;
-						String[] data = { command };
-						String loginPacket = NetworkInterface.makePacket(packetType, userId, data);
-						
-						sendMessage(loginPacket);
-					}
-				} while (running);
-				
+				runNetworkCommander(isDesigner);
 				disconnectServer();
-				
 				break;
 			case REGISTER:
-				register();		
+				register();
 				break;
 			case QUIT:
 				break;
@@ -253,12 +267,12 @@ public class Client {
 				logger.severe("Unknown network command");
 				return false;
 			}
-			
+
 			return true;
 		} else {
 			gameInterface.println("Uh-oh, The connection to server is lost.");
 		}
-		
+
 		return false;
 	}
 }
