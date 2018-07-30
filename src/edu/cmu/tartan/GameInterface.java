9b4a1d791d9696e7311f90d1622d0d6ef57fd2e7
youngwoo.jin <tomcat1983@gmail.com>
2018-07-29 22:02:20 -0400

    Add GameInterface api for server

--- src/edu/cmu/tartan/GameInterface.java
+++ src/edu/cmu/tartan/GameInterface.java
@@ -2,6 +2,8 @@ package edu.cmu.tartan;
 
 import java.util.Scanner;
 
+import edu.cmu.tartan.manager.TartanGameManager;
+
 public class GameInterface {
 
 	/**
@@ -12,7 +14,12 @@ public class GameInterface {
     /**
      * Reads input from the command line.
      */
-    private Scanner scanner;
+	private Scanner scanner;
+
+    /**
+     * Tartan Game manager for server.
+     */
+	private static TartanGameManager tartanManager;
 
 	public GameInterface() {
         scanner = new Scanner(System.in, "UTF-8");
@@ -26,6 +33,12 @@ public class GameInterface {
 		return instance;
 	}
 
+	public static boolean setGameManager(TartanGameManager manager) {
+		tartanManager = manager;
+
+		return true;
+	}
+
 	public void initialize() {
 		instance = new GameInterface();
 	}
@@ -35,12 +48,26 @@ public class GameInterface {
 	}
 
 	// For game message
-	public void print(String msg) {
-		System.out.print(msg);
+	public void print(String message) {
+		System.out.print(message);
 	}
 
-	public void println(String msg) {
-		print(msg + "\n");
+	public void println(String message) {
+		print(message + "\n");
+	}
+
+	public void print(String userId, String message) {
+		if (tartanManager == null)
+			print(message);
+		else
+			tartanManager.sendToAll(userId, message);
+	}
+
+	public void println(String userId, String message) {
+		if (tartanManager == null)
+			print(message + "\n");
+		else
+			tartanManager.sendToAll(userId, message + "\n");
 	}
 
 	public String getCommand() {
