fa1dc80c27f29e0496e7819cad7100747b1a52de
htak1 <40187539+htak1@users.noreply.github.com>
2018-07-31 09:57:57 +0900

    collect goal test

--- test/edu/cmu/tartan/goal/TestGoal.java
+++ test/edu/cmu/tartan/goal/TestGoal.java
@@ -2,24 +2,39 @@ package edu.cmu.tartan.goal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import edu.cmu.tartan.Player;
+import edu.cmu.tartan.item.Item;
 import edu.cmu.tartan.room.Room;
 
 class TestGoal {
 
 	GamePointsGoal pointGoal; 
-	private Integer winningScore = 100;  
-	private String userName = "Default";
-	Player player;
+	GameCollectGoal collectGoal; 
 
+	private Integer winningScore = 100;  
+	final static String USER_NAME = "Default";
+	Player player;
+	private ArrayList<String> itemsList = new ArrayList<>(); 
+	final static String DIAMOND = "diamond";
+	final static String FOOD = "food";
+	
 	@BeforeEach
 	public void testSetup() {
 		Room room = new Room();
-		player = new Player(room, userName);
+		player = new Player(room, USER_NAME);
 		pointGoal = new GamePointsGoal(winningScore, player);
+		
+		itemsList.clear();
+		itemsList.add(DIAMOND);
+		itemsList.add(FOOD);
+		
+		collectGoal = new GameCollectGoal(itemsList, player);
 	}
 
 
@@ -37,7 +52,7 @@ class TestGoal {
 
 	@Test
 	void testGamePointGoalStatusIncludeWinningScore() {
-		CharSequence seq = winningScore.toString();
+		CharSequence seq = "out of " + winningScore.toString();
 		boolean bool = pointGoal.getStatus().contains(seq);
 		assertTrue(bool);
 	}
@@ -76,6 +91,74 @@ class TestGoal {
 	}
 	
 	
+	@Test
+	void testGameCollectGoalDescribeIncludeDiamond() {
+		CharSequence seq = DIAMOND;
+		boolean bool = collectGoal.describe().contains(seq);
+		assertTrue(bool);
+	}
+
+	@Test
+	void testGameCollectGoalDescribeIncludeFood() {
+		CharSequence seq = FOOD;
+		boolean bool = collectGoal.describe().contains(seq);
+		assertTrue(bool);
+	}
+	
+	@Test
+	void testGameCollectGoalStatusIncludeGoalItemCount() {
+		CharSequence seq = "out of " + Integer.toString(itemsList.size()) + " items.";
+		boolean bool = collectGoal.getStatus().contains(seq);
+		assertTrue(bool);
+	}
+	
+	@Test
+	void testGameCollectGoalStatusIncludePlayerItemCount() {
+		
+		Item item = Item.getInstance(DIAMOND, USER_NAME);
+		player.pickup(item);
+		
+		CharSequence seq = "1 out of " + Integer.toString(itemsList.size()) + " items.";
+		collectGoal.isAchieved();	// to calculate get item count
+		boolean bool = collectGoal.getStatus().contains(seq);
+		
+		assertTrue(bool);
+	}
+	
+	@Test
+	void testGameCollectGoalStatusIsNotAchivvedYet() {
+		
+		Item item = Item.getInstance(DIAMOND, USER_NAME);
+		player.pickup(item);
+		
+		boolean bool = collectGoal.isAchieved();
+		assertFalse(bool);
+	}
+	
+	@Test
+	void testGameCollectGoalStatusIsAchived() {
+		
+		Item item1 = Item.getInstance(DIAMOND, USER_NAME);
+		Item item2 = Item.getInstance(FOOD, USER_NAME);
+		player.pickup(item1);
+		player.pickup(item2);
+		
+		boolean bool = collectGoal.isAchieved();
+		assertTrue(bool);
+	}
+	
+	@Test
+	void testGameCollectGoalSetPlayer() {
+		
+		GameCollectGoalSpy collectGoalSpy = new GameCollectGoalSpy(itemsList);
+		collectGoalSpy.setPlayer(player);
+		assertEquals(player, collectGoalSpy.getPlayer());
+	}
+	
+	
+	
+	
+	
 	class GamePointsGoalSpy extends GamePointsGoal {
 
 		public GamePointsGoalSpy(Integer g, Player p) {
@@ -90,5 +173,37 @@ class TestGoal {
 			return player; 
 		}
 		
+	}
+	
+	class GameCollectGoalSpy extends GameCollectGoal {
+
+		public GameCollectGoalSpy(ArrayList<String> items, Player p) {
+			super(items, p);
+		}
+		
+		public GameCollectGoalSpy(ArrayList<String> items) {
+			super(items, null);
+		}
+
+		public Player getPlayer() { 
+			return player; 
+		}
+		
+	}
+	
+	class GameExploreGoalSpy extends GameExploreGoal {
+
+		public GameExploreGoalSpy(List<String> places, Player p) {
+			super(places, p);
+		}
+		
+		public GameExploreGoalSpy(List<String> places) {
+			super(places, null);
+		}
+
+		public Player getPlayer() { 
+			return player; 
+		}
+
 	}
 }
