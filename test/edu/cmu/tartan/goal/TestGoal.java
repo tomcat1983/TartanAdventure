c3e55e1b295c967aad01188e5641e9085c4e5421
htak1 <40187539+htak1@users.noreply.github.com>
2018-07-31 10:20:39 +0900

    exlpore goal test

--- test/edu/cmu/tartan/goal/TestGoal.java
+++ test/edu/cmu/tartan/goal/TestGoal.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import edu.cmu.tartan.Player;
+import edu.cmu.tartan.TerminateGameException;
 import edu.cmu.tartan.item.Item;
 import edu.cmu.tartan.room.Room;
 
@@ -16,28 +17,41 @@ class TestGoal {
 
 	GamePointsGoal pointGoal; 
 	GameCollectGoal collectGoal; 
+	GameExploreGoal exploreGoal; 
 
 	private Integer winningScore = 100;  
 	final static String USER_NAME = "Default";
 	Player player;
 	private ArrayList<String> itemsList = new ArrayList<>(); 
+	private List<String> places = new ArrayList<>();
 	final static String DIAMOND = "diamond";
 	final static String FOOD = "food";
 	
+	final static String ROOM1 = "Room1";
+	final static String ROOM2 = "Room2";
+	private Room room;
+	
 	@BeforeEach
 	public void testSetup() {
-		Room room = new Room();
+		room = new Room();
 		player = new Player(room, USER_NAME);
 		pointGoal = new GamePointsGoal(winningScore, player);
 		
 		itemsList.clear();
 		itemsList.add(DIAMOND);
 		itemsList.add(FOOD);
-		
 		collectGoal = new GameCollectGoal(itemsList, player);
+		
+		places.clear();
+		places.add(ROOM1);
+		places.add(ROOM2);
+		exploreGoal = new GameExploreGoal(places, player);
 	}
 
-
+	/*
+	 * Game Point Goal Test 
+	 */
+	
 	@Test
 	void testGamePointGoalWinningScore() {
 		assertEquals(winningScore, pointGoal.getWinningScore());	
@@ -90,6 +104,89 @@ class TestGoal {
 		assertEquals(player, pointGoalSpy.getPlayer());
 	}
 	
+	/*
+	 * Game Explore Goal Test 
+	 */
+	
+	@Test
+	void testGameExploreGoalDescribeIncludeRoom1() {
+		CharSequence seq = ROOM1;
+		boolean bool = exploreGoal.describe().contains(seq);
+		assertTrue(bool);
+	}
+
+	@Test
+	void testGameExploreGoalDescribeIncludeRoom2() {
+		CharSequence seq = ROOM2;
+		boolean bool = exploreGoal.describe().contains(seq);
+		assertTrue(bool);
+	}
+	
+	@Test
+	void testGameExploreGoalStatusIncludeGoalRoomCount() {
+		CharSequence seq = "out of " + Integer.toString(places.size()) + " rooms.";
+		boolean bool = exploreGoal.getStatus().contains(seq);
+		assertTrue(bool);
+	}
+	
+	@Test
+	void testGameExploreGoalStatusIncludePlayerVisitCount() throws TerminateGameException {
+		
+		Room room1 = new Room(ROOM1, ROOM1);
+		PlayerSpy playerSpy = new PlayerSpy(room, USER_NAME);
+		exploreGoal.setPlayer(playerSpy);
+		playerSpy.saveRoomSpy(room1);
+		
+		CharSequence seq = "1 out of " + Integer.toString(places.size()) + " rooms.";
+		exploreGoal.isAchieved();	// to calculate get item count
+		boolean bool = exploreGoal.getStatus().contains(seq);
+		
+		System.out.println(seq + "\n"+ exploreGoal.getStatus());
+		
+		assertTrue(bool);
+	}
+	
+	@Test
+	void testGameExploreGoalStatusIsNotAchivedYet() {
+		
+		Room room1 = new Room(ROOM1, ROOM1);
+		PlayerSpy playerSpy = new PlayerSpy(room, USER_NAME);
+		exploreGoal.setPlayer(playerSpy);
+		playerSpy.saveRoomSpy(room1);
+		
+		boolean bool = exploreGoal.isAchieved();
+		assertFalse(bool);
+	}
+	
+	@Test
+	void testGameExploreGoalStatusIsAchived() {
+		
+		Room room1 = new Room(ROOM1, ROOM1);
+		Room room2 = new Room(ROOM2, ROOM2);
+
+		PlayerSpy playerSpy = new PlayerSpy(room, USER_NAME);
+		exploreGoal.setPlayer(playerSpy);
+
+		playerSpy.saveRoomSpy(room1);
+		playerSpy.saveRoomSpy(room2);
+
+		boolean bool = exploreGoal.isAchieved();
+		assertTrue(bool);
+	}
+	
+	@Test
+	void testGameExploreGoalSetPlayer() {
+		
+		GameExploreGoalSpy exploreGoalSpy = new GameExploreGoalSpy(places);
+		exploreGoalSpy.setPlayer(player);
+		assertEquals(player, exploreGoalSpy.getPlayer());
+	}
+	
+	
+	/*
+	 * Game Collect Goal Test 
+	 */
+
 	
 	@Test
 	void testGameCollectGoalDescribeIncludeDiamond() {
@@ -156,8 +253,17 @@ class TestGoal {
 	}
 	
 	
-	
-	
+	class PlayerSpy extends Player {
+
+		public PlayerSpy(Room currentRoom, String userName) {
+			super(currentRoom, userName);
+		}
+		
+		public void saveRoomSpy(Room room) {
+			this.saveRoom(room);
+		}
+		
+	}
 	
 	class GamePointsGoalSpy extends GamePointsGoal {
 
