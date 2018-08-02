8d45367cb2edb82e08a54a3af6ba2cf3d0e40033
youngwoo.jin <tomcat1983@gmail.com>
2018-08-02 17:26:33 -0400

    Modify Client test unit

--- test/edu/cmu/tartan/client/TestClient.java
+++ test/edu/cmu/tartan/client/TestClient.java
@@ -32,6 +32,8 @@ class TestClient {
 
 	@Test
 	void testWhenInputHelp() {
+		System.out.println("testWhenInputHelp");
+
 		commander.add("help");
 		commander.add("exit");
 		commander.apply();
@@ -64,6 +66,8 @@ class TestClient {
 
 	@Test
 	void testWhenInputInvalidRunningMode() {
+		System.out.println("testWhenInputInvalidRunningMode");
+
 		commander.add("55");
 		commander.add("exit");
 		commander.apply();
@@ -72,10 +76,28 @@ class TestClient {
 		client.start();
 
 		commander.destory();
+
+		assertEquals("[Tartan Adventure]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Room Escape", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Select the game mode", commander.getResultNextLine());
+		assertEquals("> 1       Local mode", commander.getResultNextLine());
+		assertEquals("> 2       Network mode", commander.getResultNextLine());
+		assertEquals("> help    What are local mode and network mode?", commander.getResultNextLine());
+		assertEquals("> exit    The game will be terminated.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Choose the mode", commander.getResultNextLine());
+		// > 55
+		assertEquals("> Invalid command : 55", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Choose the mode", commander.getResultNextLine());
 	}
 
 	@Test
 	void testWhenSelectLocalGameAndNewGame() {
+		System.out.println("testWhenSelectLocalGameAndNewGame");
+
 		commander.add("1");
 		commander.add("new");
 		commander.add("quit");
@@ -87,10 +109,66 @@ class TestClient {
 		client.start();
 
 		commander.destory();
+
+		assertEquals("[Tartan Adventure]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Room Escape", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Select the game mode", commander.getResultNextLine());
+		assertEquals("> 1       Local mode", commander.getResultNextLine());
+		assertEquals("> 2       Network mode", commander.getResultNextLine());
+		assertEquals("> help    What are local mode and network mode?", commander.getResultNextLine());
+		assertEquals("> exit    The game will be terminated.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Choose the mode", commander.getResultNextLine());
+		// > 1
+		assertEquals("> [Local mode]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Let's play :", commander.getResultNextLine());
+		assertEquals("> continue    Play the latest saved game continuously", commander.getResultNextLine());
+		assertEquals("> new         Play a new game.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Please select the game play mode.", commander.getResultNextLine());
+		// > new
+		assertEquals("> Welcome to Tartan Adventure (1.0), by Tartan Inc..", commander.getResultNextLine());
+		assertEquals("Game: This is [Collect Item] [Explore Room] [Get Point] Game", commander.getResultNextLine());
+		assertEquals("To get help type 'help' ... let's begin", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Enjoy your game!", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("You are in Room1.", commander.getResultNextLine());
+		assertEquals("You can go east to Room2. ", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("> ", commander.getResultNextLine());
+		// > quit
+		assertEquals("You collected 0 out of 2 items.", commander.getResultNextLine());
+		assertEquals("You have explored 0 out of 4 rooms.", commander.getResultNextLine());
+		assertEquals("You scored 0 out of 100 points.", commander.getResultNextLine());
+		assertEquals("Will you save the current state?(yes/no)", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		// > no
+		assertEquals("> Save is rejected.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("[Tartan Adventure]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Room Escape", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Select the game mode", commander.getResultNextLine());
+		assertEquals("> 1       Local mode", commander.getResultNextLine());
+		assertEquals("> 2       Network mode", commander.getResultNextLine());
+		assertEquals("> help    What are local mode and network mode?", commander.getResultNextLine());
+		assertEquals("> exit    The game will be terminated.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Choose the mode", commander.getResultNextLine());
+		assertEquals("> ", commander.getResultNextLine());
 	}
 
+	@Disabled
 	@Test
-	void testWhenSelectLocalGameAndContinueGame() {
+	void testWhenSelectLocalGameAndContinueGameWithoutSavedGame() {
+		System.out.println("testWhenSelectLocalGameAndContinueGameWithoutSavedGame");
+
 		commander.add("1");
 		commander.add("continue");
 		commander.add("quit");
@@ -102,12 +180,49 @@ class TestClient {
 		client.start();
 
 		commander.destory();
+
+		assertEquals("[Tartan Adventure]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Room Escape", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Select the game mode", commander.getResultNextLine());
+		assertEquals("> 1       Local mode", commander.getResultNextLine());
+		assertEquals("> 2       Network mode", commander.getResultNextLine());
+		assertEquals("> help    What are local mode and network mode?", commander.getResultNextLine());
+		assertEquals("> exit    The game will be terminated.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Choose the mode", commander.getResultNextLine());
+		// > 1
+		assertEquals("> [Local mode]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Let's play :", commander.getResultNextLine());
+		assertEquals("> continue    Play the latest saved game continuously", commander.getResultNextLine());
+		assertEquals("> new         Play a new game.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Please select the game play mode.", commander.getResultNextLine());
+		// > continue
+		assertEquals("> There is no saved game.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("[Tartan Adventure]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Room Escape", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Select the game mode", commander.getResultNextLine());
+		assertEquals("> 1       Local mode", commander.getResultNextLine());
+		assertEquals("> 2       Network mode", commander.getResultNextLine());
+		assertEquals("> help    What are local mode and network mode?", commander.getResultNextLine());
+		assertEquals("> exit    The game will be terminated.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Choose the mode", commander.getResultNextLine());
+		assertEquals("> ", commander.getResultNextLine());
 	}
 
 	@Test
-	void testWhenSelectNetworkGameMode() {
+	void testWhenSelectNetworkGameModeWithoutNetwork() {
+		System.out.println("testWhenSelectNetworkGameModeWithoutNetwork");
+
 		commander.add("2");
-		commander.add("quit");
 		commander.add("exit");
 		commander.apply();
 
@@ -115,18 +230,76 @@ class TestClient {
 		client.start();
 
 		commander.destory();
+
+		assertEquals("[Tartan Adventure]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Room Escape", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Select the game mode", commander.getResultNextLine());
+		assertEquals("> 1       Local mode", commander.getResultNextLine());
+		assertEquals("> 2       Network mode", commander.getResultNextLine());
+		assertEquals("> help    What are local mode and network mode?", commander.getResultNextLine());
+		assertEquals("> exit    The game will be terminated.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Choose the mode", commander.getResultNextLine());
+		// > 2
+		assertEquals("> I'm sorry. The game server is busy.", commander.getResultNextLine());
+		assertEquals("Please retry to connect later.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("[Tartan Adventure]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Room Escape", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Select the game mode", commander.getResultNextLine());
+		assertEquals("> 1       Local mode", commander.getResultNextLine());
+		assertEquals("> 2       Network mode", commander.getResultNextLine());
+		assertEquals("> help    What are local mode and network mode?", commander.getResultNextLine());
+		assertEquals("> exit    The game will be terminated.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Choose the mode", commander.getResultNextLine());
+		assertEquals("> ", commander.getResultNextLine());
 	}
 
 	@Disabled
 	@Test
-	void testWhenSelectDesignerMode() {
+	void testWhenSelectDesignerModeWithoutNetwork() {
+		System.out.println("testWhenSelectDesignerModeWithoutNetwork");
+
 		commander.add("99");
+		commander.add("exit");
 		commander.apply();
 
 		Client client = new Client();
 		client.start();
 
 		commander.destory();
+
+		assertEquals("[Tartan Adventure]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Room Escape", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Select the game mode", commander.getResultNextLine());
+		assertEquals("> 1       Local mode", commander.getResultNextLine());
+		assertEquals("> 2       Network mode", commander.getResultNextLine());
+		assertEquals("> help    What are local mode and network mode?", commander.getResultNextLine());
+		assertEquals("> exit    The game will be terminated.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Choose the mode", commander.getResultNextLine());
+		// > 99
+		assertEquals("> I'm sorry. The game server is busy.", commander.getResultNextLine());
+		assertEquals("Please retry to connect later.", commander.getResultNextLine());
+		assertEquals("[Tartan Adventure]", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Room Escape", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Select the game mode", commander.getResultNextLine());
+		assertEquals("> 1       Local mode", commander.getResultNextLine());
+		assertEquals("> 2       Network mode", commander.getResultNextLine());
+		assertEquals("> help    What are local mode and network mode?", commander.getResultNextLine());
+		assertEquals("> exit    The game will be terminated.", commander.getResultNextLine());
+		assertEquals("", commander.getResultNextLine());
+		assertEquals("Choose the mode", commander.getResultNextLine());
+		assertEquals("> ", commander.getResultNextLine());
 	}
 
 }
